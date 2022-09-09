0a764f668c11331391529b401945613fe6f67166
kaneh <you@example.com>
2022-09-09 15:49:43 +0200

    Ajoutez des fichiers projet.

---
+++ Compte.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Compte
+{
+    internal class Compte
+    {
+        private int numero;
+        private string nom;
+        private double solde;
+        private double decouvert;
+        public Compte(int numero, string nom, double solde, double decouvert)
+        {
+            this.numero = numero;
+            this.nom = nom;
+            this.solde = solde;
+            this.decouvert = decouvert;
+        }
+
+        public int getNumero()
+        {
+            return numero;
+        }
+        public int setNumero()
+        {
+            return this.numero;
+        }
+
+        public string getNom()
+        {
+            return nom;
+        }
+        public string setNom()
+        {
+            return this.nom;
+        }
+
+        public double getSolde()
+        {
+            return solde;
+        }
+        public double setSolde()
+        {
+            return this.solde;
+        }
+
+        public double getDecouvert()
+        {
+            return decouvert;
+        }
+        public double setDecouvert()
+        {
+            return this.decouvert;
+        }
+
+        public override string ToString()
+        {
+            return
+                    $" \n Numéro:  {getNumero()} \n Nom:  { getNom()} \n Solde: {getSolde()} \n Decouvert: {getDecouvert()}";
+        }
+
+        public bool crediter(double montant)
+        {
+
+            if(true)
+            {
+                this.solde = this.solde + montant;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Opération impossible: Votre compte n'est pas créditable"); 
+                return false;
+            }
+        }
+
+        public bool debiter(double montant)
+        {
+            if (this.solde >= montant || this.solde >= this.decouvert)
+            {
+                this.solde = this.solde - montant;
+                return true ;
+            }
+            else
+            {
+                Console.WriteLine("Opération impossible: Votre compte n'est pas débitable");
+                return false;
+            }
+        }
+
+        public void transferer(double montant, Compte compteReception)
+        {
+            
+
+            if (this.debiter(montant))
+            {
+                compteReception.crediter(montant);
+            }
+            else
+            {
+                Console.WriteLine("Opération impossible");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Compte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            /*//Test instanciation reussi
+            Compte compte = new Compte(1, "compte", 0, 0);
+            Compte compte2 = new Compte(2, "compte2", 5, 0);
+
+            //Test créditaion reussi
+            bool credit = compte.crediter(55);
+           *//* Console.WriteLine("Votre compte à été créditer de: " + credit + " €");*//*
+            Console.WriteLine("Après créditaion votre solde est de: " + compte.getSolde() + " €" + " compte");
+
+            Console.WriteLine();
+
+            //Test débitation reussi
+            bool debit = compte.debiter(25);
+           *//* Console.WriteLine("Votre compte à été débiter de: " + debit + " €");*//*
+            Console.WriteLine("Après débitation solde est de: " + compte.getSolde() + " €" + " compte");
+
+            Console.WriteLine();
+
+            //Test transfert
+            compte.transferer(30, compte2);
+            Console.WriteLine("Après créditation solde est de: " + compte2.getSolde() + " €" + " compte2");*/
+
+            Compte c1, c2;
+            c1 = new Compte(12345, "toto", 1500, -500);
+            c2 = new Compte(45657, "titi", 2000, -1000);
+            c1.transferer(1300, c2);
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine(c2.ToString());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
